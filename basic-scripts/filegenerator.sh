f4a06df21ad9d8ee9e6dcb6ebb737296f8690b37
k3shav17 <keshavrao0489@gmail.com>
2022-03-23 12:52:13 +0530

    moved some scripts to useful-scripts folder

--- basic-scripts/filegenerator.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-echo "enter the number of files you wants to generate\n enter the file name series\n file extension"
-echo "Example : 2 img_ .png"
-
-read num name ext
-
-seq $num | xargs -I {} touch $name{}.$ext
