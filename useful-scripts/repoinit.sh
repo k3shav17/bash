aac034d21f84b9beec5cba980de98cf334c435c1
k3shav17 <keshavrao0489@gmail.com>
2022-06-24 18:46:57 +0530

    script to create a repository in github from cli

---
+++ useful-scripts/repoinit.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+echo "Do you want the repo to be private or public?"
+read access
+
+echo
+echo "Creating repository with $access access"
+echo
+
+CURRENTDIR=${PWD##*/}
+
+gh repo create $CURRENTDIR --$access
+
+echo "# ${CURRENTDIR}" >> README.md
+git init
+git add README.md
+git commit -m "initial commit"
+git branch -M main
+git remote add origin git@github.com:k3shav17/${CURRENTDIR}.git
+git push -u origin main
+
+echo
+echo "Repository has been initialized successfully"
+
+
+
