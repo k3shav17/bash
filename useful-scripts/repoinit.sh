b558852e0ed443ffc0eb6ebc883c8b5fcc89287a
k3shav17 <keshavrao0489@gmail.com>
2022-08-29 16:26:47 +0530

    comments in upscript and recommend shellcheck changes

--- useful-scripts/repoinit.sh
+++ useful-scripts/repoinit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 echo "Do you want the repo to be private or public?"
-read access
+read -r access
 
 echo
 echo "Creating repository with $access access"
@@ -9,14 +9,14 @@ echo
 
 CURRENTDIR=${PWD##*/}
 
-gh repo create $CURRENTDIR --$access
+gh repo create "$CURRENTDIR" --"$access"
 
 echo "# ${CURRENTDIR}" >> README.md
 git init
 git add README.md
 git commit -m "initial commit"
 git branch -M main
-git remote add origin git@github.com:k3shav17/${CURRENTDIR}.git
+git remote add origin git@github.com:k3shav17/"${CURRENTDIR}".git
 git push -u origin main
 
 echo
